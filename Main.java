26a6a72bda5c2c63e2433bee17e7919e23d20dc1
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-11-07 00:04:16 +0530

    Added:Welcome Message

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.LineComparisonProblem;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program"); 
+		
+	}
+
+}
